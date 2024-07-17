d5cdbb3b59abeb62297f80b5daa51556a838f38f
Nathaniel Alcedo <natealcedo@gmail.com>
2024-07-17 18:17:29 +0800

    Move interfaces into its own package

--- interfaces/service.go
+++ interfaces/service.go
@@ -1,8 +1,9 @@
-package services
+package interfaces
 
 type GenericService interface {
 	Create(body interface{}) (interface{}, error)
 	GetAll() ([]interface{}, error)
-	GetByID(id string, model interface{}, relationships []string) (interface{}, error)
+	GetByID(id string) (interface{}, error)
 	DeleteByID(id string) error
+	Relationships() []string
 }
